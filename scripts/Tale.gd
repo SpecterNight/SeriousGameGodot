af2bf82889ea88f3c10fc5b36052b214d3edd568
SpecterNight <jainerpinta25@gmail.com>
2024-10-06 23:05:40 -0500

    Migrate to GDScript

---
+++ scripts/Tale.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var is_response_processed: bool = false
+var variables: Node
+var tale_data: Dictionary
+var dialogue_component: PackedScene = preload("res://components/Dialogue.tscn")
+
+func _ready():
+    variables = get_node("/root/Variables")
+    get_tale()
+
+func _process(delta):
+    pass
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+    if is_response_processed:
+        return
+    is_response_processed = true
+    var json = JSON.parse_string(body.get_string_from_utf8())
+    tale_data = json
+    render_dialogues()
+
+func get_tale():
+    var http_request = $HTTPRequest
+    http_request.request(variables.URL_Back + "/tale/id/" + variables.tale_id, ["Cookie:token=" + variables.cookie])
+
+func render_dialogues() -> void:
+    var title = $Title
+    title.text = tale_data["name"]
+    var dialogues = tale_data["dialogues"]
+
+    var timer = $Timer
+    for dialogue in dialogues:
+        var dialogue_instance = dialogue_component.instance()
+        dialogue_instance.get_nodes()
+        var character = dialogue["character"]
+
+        dialogue_instance.set_data(character["name"], character["urlImage"], dialogue["text"])
+        $MarginContainer/ScrollContainer/DialogueVB.add_child(dialogue_instance)
+        timer.start(2.0)
+        await timer.timeout
---
+++ scripts/Variables.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var cookie: String
+var URL_Back: String = "http://localhost:3006/api/v1"
+var tale_id: String
+
+var result: Array = []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#URL_Back = "http://localhost:3006/api/v1"eplace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
